850ba1b9b1ef96bbba6b72124b726a5121276cc6
Naitoshedo <hozuki@naitoshedo.tv>
2025-05-29 12:23:04 -0500

    Update gen-version.sh

--- gen-version.sh
+++ gen-version.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+cd ../../
 git describe --abbrev=0 --tags
 rm -f qmk_version.h
-echo "#define QMK_VERSION_BYTE_1 `(git describe --tags | cut -d. -f1)`" >> qmk_version.h
-echo "#define QMK_VERSION_BYTE_2 `(git describe --tags | cut -d. -f2)`" >> qmk_version.h
-echo "#define QMK_VERSION_BYTE_3 `(git describe --tags | cut -d. -f3 | cut -d- -f1)`" >> qmk_version.h
+echo "#define QMK_VERSION_BYTE_1 `(git describe --tags | cut -d. -f1)`" >> modules/signalrgb/qmk_version.h
+echo "#define QMK_VERSION_BYTE_2 `(git describe --tags | cut -d. -f2)`" >> modules/signalrgb/qmk_version.h
+echo "#define QMK_VERSION_BYTE_3 `(git describe --tags | cut -d. -f3 | cut -d- -f1)`" >> modules/signalrgb/qmk_version.h
